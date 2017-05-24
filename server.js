5c61f5ebab46417dc02d61d2769112d349b447c2
Phil Ershadi <phildoge@gmail.com>
2017-05-24 14:57:13 -0400

    Got everything ready for heroku!

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 3000;
+//listens for a process environment variable, but if there isn't one, defaults to 3000.
+//use const so it can't be changed once it's running.
+
+//express middleware to let you modify the requests being sent.
+app.use(function(request, response, next){
+    if (request.headers['x-forwarded-proto'] === "http"){
+        next();
+    }else{
+        response.redirect("http://" + request.hostname + request.url);
+    }
+})
 
 // This lets express see folders that are in the directory listed in the argument
 //Index.html goes here
 app.use(express.static('public'));
 
 //This tells the server to start
-app.listen(3000,function(){console.log("We are LIVE on port 3000!");});
+app.listen(PORT,function(){console.log("We are LIVE on port " + PORT + "!");});
