d066438ebbb3ecf5501728e92663b032ec4ed992
Phil Ershadi <phildoge@gmail.com>
2017-06-02 10:51:42 -0400

    Changed modal error

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require("react");
+var ReactDOM = require("react-dom");
+var ReactDOMServer = require("react-dom/server");
 
 var ErrorModal = React.createClass({
    
@@ -12,19 +14,30 @@ var ErrorModal = React.createClass({
         message:React.PropTypes.string.isRequired
     },
     componentDidMount:function(){
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-
-    },
-    render:function(){
         var {title,message} = this.props;
-        return(
+        var modalMarkup = (
             <div id = "error-modal" className="reveal tiny text-center" data-reveal ="">
               <h4>{title}</h4>
               <p className="lead">{message}</p>
               <p> <button className="button hollow expanded" data-close = "">...Fine.</button></p>
             </div>
         );
+
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup)); //changes your markup to a form usable by jquery
+        $(ReactDOM.findDOMNode(this)).html($modal); //uses jquery to change the modal message to whatever the modal markup is
+
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+
+    },
+    render:function(){
+       
+//render nothing to the dom, but leave this as a placeholder so we can change it with jquery
+        return(
+            <div>
+            </div>
+        )
+
     }
 
 });
